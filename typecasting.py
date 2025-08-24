b98d369b629dd17c511e77b9bf10d9d52c8e37ef
krishna-mankad27 <krishnamankhand02@gmail.com>
2025-08-24 11:26:54 +0530

    idk

---
+++ typecasting.py
@@ -0,0 +1,11 @@
+#typecasting ... type(),int(),bool(),float(),str()
+name = "krishna"
+emptystr = ""
+age = 19
+sgpa = 7.5
+is_student = True
+print(type(name),type(age),type(sgpa),type(is_student))
+print(int(sgpa))
+print(float(age))
+print(bool(name),bool(emptystr))
+print(int(name))#error -> value error
