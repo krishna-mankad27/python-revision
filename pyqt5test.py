7a965e2d3e7f9d28e69889ca6df75fd53cd0b7ae
krishna-mankad27 <krishnamankhand02@gmail.com>
2025-09-09 21:32:58 +0530

    9/9/25-1

--- pyqt5test.py
+++ pyqt5test.py
@@ -8,10 +8,10 @@ class mainwindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("hello world")
-        self.setGeometry(0,0,500,500)
+        self.setGeometry(0,0,1000,1000)
         self.setWindowIcon(QIcon("image.png"))
         label = QLabel("hello, how are you",self)
-        label.setGeometry(0,0,300,300)
+        label.setGeometry(700,0,300,300)
         label.setStyleSheet("color: #fc03e8;""background-color: #c3f7ca;""font-style : italic;""font-weight : bold;""text-decoration : underline;")
         label.setFont(QFont("Arial",10))
         label.setAlignment(Qt.AlignTop)
@@ -19,6 +19,10 @@ class mainwindow(QMainWindow):
         pixmap = QPixmap("intel.jpg")
         label.setPixmap(pixmap)
         label.setScaledContents(True)
+        labe2 = QLabel("hello, how are you",self)
+        labe2.setStyleSheet("color: #fc03e8;""background-color: #c3f7ca;""font-style : italic;""font-weight : bold;""text-decoration : underline;")
+        labe2.setFont(QFont("Arial",25))
+        labe2.setGeometry(0,0,700,150)
 #        label.setAlignment(Qt.AlignBottom)
 def main():
     app = QApplication(sys.argv)
